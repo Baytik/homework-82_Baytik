3af16ddcc1a35a9ba6adfba094eacecc8c098bd6
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-03-03 18:06:04 +0600

    Закончил с Треками и вместе с ним и задание

--- app/track.js
+++ app/track.js
@@ -4,8 +4,13 @@ const Track = require('../models/Track');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    if (req.query.album) {
+        const tracks = await Track.find({album: req.query.album});
+        res.send(tracks)
+    } else {
     const tracks = await Track.find();
-    res.send(tracks)
+    res.send(tracks);
+    }
 });
 
 module.exports = router;
